f7fdd29784720e124f1716f7be7a09c5a069b2b9
extrusiones <tabares2p@gmail.com>
2024-03-27 12:29:13 -0500

    mod2

--- src/cfp/GenerateInfoFiles.java
+++ src/cfp/GenerateInfoFiles.java
@@ -7,34 +7,34 @@ import java.util.Random;
 public class GenerateInfoFiles {
 
 		
-		public static void createSalesMenFile(int randomSalesCount, String name, long id) {
+		public static void crearArchivoVendedor(int randomSalesCount, String name, long id) {
 	        try {
-	            FileWriter writer = new FileWriter( name + "_" + id + "_sales.txt");
+	            FileWriter writer = new FileWriter( name + "_" + id + ".txt");
 	            Random random = new Random();
 	            for (int i = 0; i < randomSalesCount; i++) {
-	                int saleAmount = random.nextInt(1000); // Generate pseudo-random sales amount
-	                writer.write("Sale: " + (i + 1) + ", Amount: $" + saleAmount + "\n");
+	                int saleAmount = random.nextInt(1000); 
+	                writer.write("Venta: " + (i + 1) + ", Cantidad: $" + saleAmount + "\n");
 	            }
 	            writer.close();
-	            System.out.println("Sales file for " + name + " with ID " + id + " created successfully.");
+	            System.out.println("Vendedor creado exitosa mente");
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
 	    }
 		
 		
-		public static void createProductsFile(int productsCount) {
+		public static void CrearArchivoProductos(int productsCount) {
 	        String[] products = {"Product A", "Product B", "Product C", "Product D", "Product E"};
 	        try {
-	            FileWriter writer = new FileWriter("products.txt");
+	            FileWriter writer = new FileWriter("Productos.txt");
 	            Random random = new Random();
 	            for (int i = 0; i < productsCount; i++) {
 	                String productName = products[random.nextInt(products.length)];
 	                double price = random.nextDouble() * 100; // Generate pseudo-random price
-	                writer.write("Product: " + productName + ", Price: $" + price + "\n");
+	                writer.write("Producto: " + productName + ", Precio: $" + price + "\n");
 	            }
 	            writer.close();
-	            System.out.println("Products file created successfully.");
+	            System.out.println("productos creados exitosa mente");
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
@@ -43,17 +43,17 @@ public class GenerateInfoFiles {
 		
 		 private static final String[] NAMES = {"John", "Emma", "Michael", "Sophia", "William", "Olivia"};
 
-		    public static void createSalesManInfoFile(int salesmanCount) {
+		    public static void ArchivoInfoVendedor(int salesmanCount) {
 		        try {
-		            FileWriter writer = new FileWriter( "salesmen.txt");
+		            FileWriter writer = new FileWriter( "Vendedores.txt");
 		            Random random = new Random();
 		            for (int i = 0; i < salesmanCount; i++) {
 		                String name = NAMES[random.nextInt(NAMES.length)];
 		                long id = Math.abs(random.nextLong()); // Generate pseudo-random ID
-		                writer.write("Name: " + name + ", ID: " + id + "\n");
+		                writer.write("Nombre: " + name + ", ID: " + id + "\n");
 		            }
 		            writer.close();
-		            System.out.println("Salesmen info file created successfully.");
+		            System.out.println("Se creo exitoa mente ");
 		        } catch (IOException e) {
 		            e.printStackTrace();
 		        }
@@ -61,9 +61,9 @@ public class GenerateInfoFiles {
 
 
 	    public static void main(String[] args) {
-	        createSalesMenFile(10, "John", 123456789);
-	        createProductsFile(5);
-	        createSalesManInfoFile(3);
+	    	crearArchivoVendedor(10, "John", 123456789);
+	    	CrearArchivoProductos(5);
+	    	ArchivoInfoVendedor(3);
 	    }
 	}
 
